e3c84d6644514e6adcd0bb51dc48255040c7b8ba
little-en-grey <nightcat.koh@gmail.com>
2020-08-30 14:13:30 +0900

    Update JavaScript.js

--- Status/js/JavaScript.js
+++ Status/js/JavaScript.js
@@ -73,10 +73,33 @@ $(document).ready(function() {
 
  });
 
+ const outputElement = document.getElementById('output_csv');
 
-//Flex Slider
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    directionNav: false
+ function getCsvData(dataPath) {
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', (event) => {
+   const response = event.target.responseText;
+   convertArray(response);
   });
-});
+  request.open('GET', dataPath, true);
+  request.send();
+ }
+
+ function convertArray(data) {
+  const dataArray = [];
+  const dataString = data.split('n');
+  for (let i = 0; i < dataString.length; i++) {
+   dataArray[i] = dataString[i].split(',');
+  }
+  let insertElement = '';
+  dataArray.forEach((element) => {
+   insertElement += '<tr>';
+   element.forEach((childElement) => {
+    insertElement += `<td>${childElement}</td>`
+   });
+   insertElement += '</tr>';
+  });
+  outputElement.innerHTML = insertElement;
+ }
+
+ getCsvData('./ステータス一覧.csv');
