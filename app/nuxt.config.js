5e9ae3d984d421f0c2108a3f4003868b07f4d39c
little_en_grey <little.en.grey@gmail.com>
2023-07-25 12:30:05 +0900

    commit

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - we',
+    titleTemplate: '%s - little en grey',
     title: 'Profile',
     meta: [
       { charset: 'utf-8' },
